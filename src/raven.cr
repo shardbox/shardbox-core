32c794f68e46f7cf81125036d69f030fd726aa1e
Johannes Müller <straightshoota@gmail.com>
2019-10-30 14:57:09 +0100

    Add SENTRY_DSN_VAR

--- src/raven.cr
+++ src/raven.cr
@@ -3,4 +3,8 @@ require "raven"
 Raven.configure do |config|
   config.connect_timeout = 10.seconds
   config.current_environment = "production"
+
+  if env_var = ENV["SENTRY_DSN_VAR"]?
+    config.dsn = ENV[env_var]
+  end
 end
