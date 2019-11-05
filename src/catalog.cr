af7780b13a720b188c1b3d66deb4eb430de41eb3
Johannes Müller <straightshoota@gmail.com>
2019-11-05 15:28:07 +0100

    Add Catalog::Mirror

--- src/catalog.cr
+++ src/catalog.cr
@@ -37,23 +37,103 @@ module Catalog
     end
   end
 
+  struct Mirror
+    include JSON::Serializable
+
+    enum State
+      MIRROR
+      LEGACY
+    end
+
+    getter repo_ref : Repo::Ref
+    property? state : State
+
+    def initialize(@repo_ref : Repo::Ref, @state : State = :mirror)
+    end
+
+    def legacy?
+      state.legacy?
+    end
+
+    def to_yaml(builder)
+      builder.mapping do
+        builder.scalar repo_ref.resolver
+        builder.scalar repo_ref.url.to_s
+
+        if legacy?
+          builder.scalar "state"
+          role.to_yaml(builder)
+        end
+      end
+    end
+
+    def self.new(ctx : YAML::ParseContext, node : YAML::Nodes::Node)
+      unless node.is_a?(YAML::Nodes::Mapping)
+        node.raise "Expected mapping, not #{node.class}"
+      end
+
+      repo_ref = nil
+      state = nil
+
+      YAML::Schema::Core.each(node) do |key, value|
+        key = String.new(ctx, key)
+        case key
+        when "state"
+          unless value.is_a?(YAML::Nodes::Scalar) && (state = State.parse?(value.value))
+            raise %(unexpected value for key `state` in Category::Mirror mapping, allowed values: #{State.values})
+          end
+        else
+          repo_ref = Entry.parse_repo_ref(ctx, key, value)
+
+          unless repo_ref
+            node.raise "unknown key: #{key} in Category::Mirror mapping"
+          end
+        end
+      end
+
+      unless repo_ref
+        node.raise "missing required repo reference"
+      end
+
+      state ||= State::MIRROR
+      new(repo_ref, state)
+    end
+  end
+
   struct Entry
     include JSON::Serializable
+
+    enum State
+      ACTIVE
+      ARCHIVED
+
+      def to_s(io : IO)
+        io << to_s
+      end
+
+      def to_s
+        super.downcase
+      end
+
+      def to_yaml(builder : YAML::Nodes::Builder)
+        builder.scalar to_s
+      end
+    end
 
     getter repo_ref : Repo::Ref
     property description : String?
     getter mirror : Array(Repo::Ref)
     getter legacy : Array(Repo::Ref)
     getter categories : Array(String)
-    property state : String?
+    property state : State
 
     def initialize(@repo_ref : Repo::Ref, @description : String? = nil,
                    @mirror = [] of Repo::Ref, @legacy = [] of Repo::Ref,
-                   @state : String? = nil, @categories = [] of String)
+                   @state : State = :active, @categories = [] of String)
     end
 
     def archived? : Bool
-      state == "archived"
+      state.archived?
     end
 
     def to_yaml(builder : YAML::Nodes::Builder)
@@ -69,9 +149,9 @@ module Catalog
         mirror_to_yaml(builder, mirror, "mirror")
         mirror_to_yaml(builder, legacy, "legacy")
 
-        if state = @state
+        if archived?
           builder.scalar "state"
-          builder.scalar state
+          state.to_yaml(builder)
         end
       end
     end
@@ -81,10 +161,7 @@ module Catalog
         builder.scalar name
         builder.sequence do
           list.each do |ref|
-            builder.mapping do
-              builder.scalar ref.resolver
-              builder.scalar ref.url.to_s
-            end
+            ref.to_json(builder)
           end
         end
       end
@@ -107,20 +184,12 @@ module Catalog
         when "description"
           description = String.new(ctx, value)
         when "mirror"
-          unless value.is_a?(YAML::Nodes::Sequence)
-            raise "expected sequence for key `mirror` in Category::Entry mapping"
-          end
-          mirror = parse_mirror_or_legacy(ctx, value)
+          mirror = Array(Mirror).new(ctx, value).map(&.repo_ref)
         when "legacy"
-          unless value.is_a?(YAML::Nodes::Sequence)
-            raise "expected sequence for key `legacy` in Category::Entry mapping"
-          end
-          legacy = parse_mirror_or_legacy(ctx, value)
+          legacy = Array(Mirror).new(ctx, value).map(&.repo_ref)
         when "state"
-          if value.is_a?(YAML::Nodes::Scalar) && value.value == "archived"
-            state = value.value
-          else
-            raise %(unexpected value for key `state` in Category::Entry mapping, allowed values: "archived")
+          unless value.is_a?(YAML::Nodes::Scalar) && (state = State.parse?(value.value))
+            raise %(unexpected value for key `state` in Category::Entry mapping, allowed values: #{State.values})
           end
         else
           repo_ref = parse_repo_ref(ctx, key, value)
@@ -135,28 +204,11 @@ module Catalog
         node.raise "missing required repo reference"
       end
 
+      state ||= State::ACTIVE
       new(repo_ref, description, mirror, legacy, state)
     end
 
-    private def self.parse_mirror_or_legacy(ctx, node)
-      list = [] of Repo::Ref
-      node.each do |child|
-        unless child.is_a?(YAML::Nodes::Mapping)
-          child.raise "Expected mapping, not #{child.class} in mirror entry"
-        end
-        YAML::Schema::Core.each(child) do |key, value|
-          key = String.new(ctx, key)
-          parsed_ref = parse_repo_ref(ctx, key, value)
-          unless parsed_ref
-            raise "unknown key: #{key} in mirror mapping"
-          end
-          list << parsed_ref
-        end
-      end
-      list
-    end
-
-    private def self.parse_repo_ref(ctx, key, value)
+    def self.parse_repo_ref(ctx : YAML::ParseContext, key, value)
       if key == "git"
         # Special case "git" to resolve URLs pointing at named service providers (like https://github.com/foo/foo)
         Repo::Ref.new(String.new(ctx, value))
