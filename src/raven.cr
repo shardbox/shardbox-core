49b9b1ce1ece80c6f880203ce10a061ce92df6f9
Johannes Müller <straightshoota@gmail.com>
2019-11-21 14:15:34 +0100

    Fix Raven environment to use CRYSTAL_ENV

--- src/raven.cr
+++ src/raven.cr
@@ -2,7 +2,8 @@ require "raven"
 
 Raven.configure do |config|
   config.connect_timeout = 10.seconds
-  config.current_environment = "production"
+
+  config.current_environment = ENV["CRYSTAL_ENV"]? || "development"
 
   if env_var = ENV["SENTRY_DSN_VAR"]?
     config.dsn = ENV[env_var]
